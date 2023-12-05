0eca8125ea0ed761f0b7270c5f45daad3bd5cb71
Anila Kodukula <anilakodukula@gmail.com>
2023-12-05 14:50:57 -0500

    increment counter

--- 1. Build a Passenger Counter App/09. Using functions to write less code/index.js
+++ 1. Build a Passenger Counter App/09. Using functions to write less code/index.js
@@ -12,8 +12,12 @@ console.log(1)
 
 // Get ready for a new race 🏎 🏎 🏎
 
-console.log(5)
-console.log(4)
-console.log(3)
-console.log(2)
-console.log(1)
+let count = 5;
+
+const timer = setInterval(function() {
+    count--;
+    console.log(count);
+    if (count == 1){
+        console.log("GO!");
+    }
+}, 1000)
